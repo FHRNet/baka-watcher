f211beeb5f77de5de1756675df04aefce4f9efe5
Filip Hruska <fhr@iMac.local>
2017-05-18 19:32:27 +0200

    Fixes #3

--- web.py
+++ web.py
@@ -9,6 +9,7 @@ import re
 import urllib2
 import hashlib
 import binascii
+import datetime
 
 from email.utils import parseaddr
 from google.appengine.ext import ndb
@@ -17,12 +18,32 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 class SendMail:
     client = None
-    
+
     def __init__(self):
         self.client = sendgrid.SendGridClient('sg_login', 'sg_pass')
 
+    def sendConfirmation(self, email, key):
+        s = self.client
+        # SendGrid API credentials
+        message = sendgrid.Mail()
+
+        # From address
+        message.set_from('skola@example.com')
+        message.add_to(email)
+        # Subject of sent emails
+        message.set_subject('Potvrzení přihlášení do aplikace GBL Watcher')
+        message.add_filter('templates', 'enable', '1')
+        # SendGrid template id
+        message.add_filter('templates', 'template_id', 'xxxxx')
+        # Pass variables to template
+        message.add_substitution(':key', key)
+        message.set_html("data")
+        status, msg = s.send(message)
+        print status, msg
+
     def send(self, email, name, html, unsub):
         s = self.client
+        # SendGrid API credentials
         message = sendgrid.Mail()
 
         # From address
@@ -38,13 +59,14 @@ class SendMail:
         message.add_substitution(':unsub', unsub)
         message.set_html(html)
         status, msg = s.send(message)
-        
 
 
 # Database models
 class Subscribers(ndb.Model):
     email = ndb.StringProperty()
     name = ndb.StringProperty()
+    added = ndb.DateTimeProperty()
+    confirmed = ndb.BooleanProperty(default=True)
 
 class Misc(ndb.Model):
     k = ndb.StringProperty()
@@ -54,16 +76,17 @@ class Misc(ndb.Model):
 class Main(object):
     data = None
     sm = None
-    
-    @cherrypy.expose(['send'])
+
     def send(self, email, name):
         # Generate unsub key
-        unsub = "email=" + email + "&key=" + self.unsubKey(email)
+        unsub = "email=" + email + "&key=" + self.uniqueKey(email)
+
         self.sm.send(email, name, self.data, unsub)
         return("OK")
 
     @cherrypy.expose(['subscribe'])
     def subscribe(self, name, email):
+        self.sm = SendMail()
         if name == "":
             # User has not filled in his name, fall back to this value
             name = "člověče"
@@ -75,16 +98,31 @@ class Main(object):
             return("Already exists")
         else:
             # Yaay, everything is fine. Add them to Subscribers table.
-            newsub = Subscribers(email=email, name=name)
+            newsub = Subscribers(email=email, name=name, confirmed=False, added=self.curDate())
             newsub_store = newsub.put()
-            self.send(email, name)
+            self.sm.sendConfirmation(email, ("email=" + email + "&key=" + self.uniqueKey(email)))
             return("Stored")
+
+    @cherrypy.expose
+    def confirm(self, email, key):
+        if(key == self.uniqueKey(email)):
+            try:
+                sub = Subscribers.query(Subscribers.email == email).get()
+                if(sub.confirmed):
+                    return("Tato adresa je již potvrzena")
+                else:
+                    sub.confirmed = True
+                    sub.put()
+                    return("Potvrzeno.")
+            except:
+                return("Tento link již není platný. Pokud máš stále zájem, registruj se znovu.")
+
 
     @cherrypy.expose(['unsub'])
     def unsub(self, email, key):
         # User wants to unsub :/
         # But is he real?
-        if(key == self.unsubKey(email)):
+        if(key == self.uniqueKey(email)):
             # He sure is. Delete them.
             ndb.delete_multi(Subscribers.query(Subscribers.email == email).fetch(keys_only=True))
             return("OK")
@@ -93,12 +131,13 @@ class Main(object):
             return("Access denied")
 
     def sendToSubs(self):
+        self.sm = SendMail()
         # Get everyone from database
         qry = Subscribers.query().fetch()
         # Each user wants to get an email
-        self.sm = SendMail()
         for sub in qry:
-            self.send(sub.email, sub.name)
+            if sub.confirmed:
+                self.send(sub.email, sub.name)
         return("OK")
 
     @cherrypy.expose
@@ -115,8 +154,21 @@ class Main(object):
         print("Fetching a fresh copy of data")
         self.data = self.suplovani()
 
-    def unsubKey(self, email):
+    def uniqueKey(self, email):
         return(hashlib.sha1(email + b'MY_PRECIOUS').hexdigest()) # Unsubscribe key generator with salt
+
+    def curDate(self):
+        return(datetime.datetime.now())
+
+    @cherrypy.expose
+    def clean(self):
+        # We want to clean non-activated accounts after a period of time
+        qry = Subscribers.query().fetch()
+        for sub in qry:
+            if not sub.confirmed:
+                delta = (self.curDate() - sub.added)
+                if (delta.seconds > (86400*2)):
+                    ndb.delete_multi(Subscribers.query(Subscribers.email == sub.email).fetch(keys_only=True))
 
     @cherrypy.expose
     def hasChanged(self):
@@ -142,6 +194,7 @@ class Main(object):
             m.put()
             self.sendToSubs()
             return("Updated")
+
 
 
 # CherryPy config
